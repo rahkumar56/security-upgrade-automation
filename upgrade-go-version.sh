6c536299c15ade04e16b584ec3e82c9825fe95b9
rahkumar56 <139110109+rahkumar56@users.noreply.github.com>
2024-01-29 16:37:53 +0530

    Update upgrade-go-version.sh

--- upgrade-go-version.sh
+++ upgrade-go-version.sh
@@ -177,14 +177,14 @@ for repo in "${repositories[@]}"; do
 
      url='https://api.github.com/repos/'$repo_owner'/'$repo_name'/pulls'
     echo $url
-    body='{ "title": "Updated go version", "body": "Please pull these awesome changes in!", "head": "'$repo_owner':'$feature_branch'",  "base": "'$base_ranch'" }'
+    body='{ "title":"Updated go version", "body":"Please pull these awesome changes in!", "head":"'$repo_owner':'$feature_branch'", "base":"'$base_ranch'" }'
     echo $body
     echo $pat_token
-    curl --verbose --location $url \
+    curl --verbose --location "$url" \
     --header 'Accept: application/vnd.github+json' \
-    --header 'Authorization: Bearer '$pat_token \
+    --header "Authorization: Bearer $pat_token" \
     --header 'Content-Type: application/json' \
-    --data $body
+    --data "$body"
 
     # # curl --location 'https://api.github.com/repos/$repo_owner/$repo_name/pulls' \
     # # --header 'Accept: application/vnd.github+json' \
