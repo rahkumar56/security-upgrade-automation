7d08d72b2a05898bce9c0e159e790c7cb257391b
rahkumar56 <139110109+rahkumar56@users.noreply.github.com>
2024-02-05 17:31:59 +0530

    Update update-ci-config.sh

--- update-ci-config.sh
+++ update-ci-config.sh
@@ -289,6 +289,9 @@ commit_generate_pr(){
     # Parse the latest version from the response using jq
     pr_url=$(echo "$curl_response" | jq -r '.url')
 
+    curl -X POST --data-urlencode "payload={\"channel\": \"<+pipeline.variables.channel_name>
+    \", \"username\": \"Security-Automation\",\"type\": \"mrkdwn\", \"text\":\"*Security Automation : PR Is generated for ci-manager-config.yml file :* :white_check_mark: :$pr_url", \"icon_emoji\": \":harnesshd:\"}" https://hooks.slack.com/services/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
+
     #echo "***************Ended Execution for the repo: $repo_name \n\n PR url: $pr_url"
     echo $pr_url
 
