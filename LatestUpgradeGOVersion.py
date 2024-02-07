329262b55e148267327dcba85c98fa37bb91aa3f
rahkumar56 <139110109+rahkumar56@users.noreply.github.com>
2024-02-07 14:39:03 +0530

    Update LatestUpgradeGOVersion.py

--- LatestUpgradeGOVersion.py
+++ LatestUpgradeGOVersion.py
@@ -225,9 +225,10 @@ def create_pull_request(repo_path, feature_branch, base_branch, title, body):
     
     subprocess.run(["git", "config", "--global", "user.email", "rahul.kumar@harness.io"])
     subprocess.run(["git", "config", "--global", "user.name", "rahkumar56"])
-    remote_url = f"https://rahkumar56:{pat_token}@github.com/{repo_owner}/{repo_name}"
+    remote_url = f"https://rahkumar56:{pat_token}@github.com/{repo_owner}/{repo_name}.git"
     print(f"Remote repo url::{remote_url}")
     subprocess.run(["git", "remote", "set-url", "origin", remote_url])
+    repo.git.remote("set-url", "origin", remote_url)
     # Create a new branch
     # repo.git.checkout(base_branch)
     # repo.git.pull()
